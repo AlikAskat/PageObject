204b4720e0d831da30b1d17044f44be9d8da3577
AlikAskat <a-r-b@bk.ru>
2020-08-05 13:21:15 +0500

    Update DataHelper.java

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -3,7 +3,6 @@ package ru.netology.data;
 import lombok.Value;
 
 public class DataHelper {
-    public static AuthInfo original;
     public static AuthInfo authInfo;
 
     private DataHelper() {}
@@ -16,11 +15,6 @@ public class DataHelper {
 
     public static AuthInfo getAuthInfo() {
         return new AuthInfo("vasya", "qwerty123");
-    }
-
-    public static AuthInfo getOtherAuthInfo(AuthInfo original) {
-        DataHelper.original = original;
-        return new AuthInfo("petya", "123qwerty");
     }
 
     @Value
