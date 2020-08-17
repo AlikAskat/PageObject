fa6b5a204d7e5928bf3f65718f310028d5639cf4
AlikAskat <a-r-b@bk.ru>
2020-08-18 00:40:12 +0500

    Update MoneyTransferTest.java

--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -16,7 +16,7 @@ public class MoneyTransferTest {
         val authInfo = DataHelper.getAuthInfo();
         val verificationPage = loginPage.validLogin( authInfo );
         val verificationCode = DataHelper.getVerificationCodeFor( authInfo );
-        val dashboardPage = verificationPage.validVerify( verificationCode );
+        verificationPage.validVerify( verificationCode );
     }
 
 }
