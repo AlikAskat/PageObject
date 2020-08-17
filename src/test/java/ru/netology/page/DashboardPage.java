a789dfb21e25e333166a6369330d8cd3c4ec070a
AlikAskat <a-r-b@bk.ru>
2020-08-17 13:09:45 +0500

    Update DashboardPage.java

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -8,25 +8,33 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.impl.Html.text;
+import static java.lang.String.*;
 
 public class DashboardPage {
     private ElementsCollection cards = $$(".list__item");
     private String balanceStart = "баланс: ";
-    private String extractBalance;
-    private final String balanceFinish = " р.";
+    private String balanceFinish = " р.";
+    private String id;
 
     public DashboardPage() {
         SelenideElement heading = $( "[data-test-id='dashboard']" );
         heading.shouldBe(visible);
     }
 
-    public int getBalanceStart(String balanceStart, String extractBalance) {
-        this.balanceStart = balanceStart;
-        this.extractBalance = extractBalance;
+//    public int getBalanceStart() {
+//        return getBalanceStart( "*[@id='root']/div/ul/li[1]/div/text()[3]", "*[@id='root']/div/ul/li[2]/div/text()[3]" );
+//    }
+
+    private int getCardBalance(String id) {
+        this.id = id;
+//        this.balanceStart = balanceStart;
+//        this.extractBalance = extractBalance;
 //        private balanceStart = new balanceStart();
-        $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']");
-        $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']");
-        return extractBalance( String.valueOf( text ) );
+        $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']").click();
+//        $( "#root > div > ul > li:nth-child(1) > div > button" ).click();
+        $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']").click();
+//        $( "#root > div > ul > li:nth-child(1) > div > button" ).click();
+        return extractBalance( valueOf( text ) );
     }
 //    public String getBalanceFinish() {
 //        return balanceFinish;
