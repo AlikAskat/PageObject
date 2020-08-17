f189cdf34ef67d97e7db462bceff1cc71530310c
AlikAskat <a-r-b@bk.ru>
2020-08-18 00:30:03 +0500

    Update DashboardPage.java

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -15,35 +15,21 @@ public class DashboardPage {
     private final String balanceStart = "баланс: ";
     private final String balanceFinish = " р.";
     public String id;
-    private Object DashboardPage;
+
 
 
     public DashboardPage() {
         SelenideElement heading = $( "[data-test-id='dashboard']" );
         heading.shouldBe(visible);
     }
-    public void dashboardId() {
-        val dashboardPage = new DashboardPage();
-    }
 
-//    public int getBalanceStart() {
-//        return getBalanceStart( "*[@id='root']/div/ul/li[1]/div/text()[3]", "*[@id='root']/div/ul/li[2]/div/text()[3]" );
-//    }
 
     private int getCardBalance(String id) {
         this.id = id;
-//        this.balanceStart = balanceStart;
-//        this.extractBalance = extractBalance;
-//        private balanceStart = new balanceStart();
         $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']").click();
-//        $( "#root > div > ul > li:nth-child(1) > div > button" ).click();
         $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']").click();
-//        $( "#root > div > ul > li:nth-child(1) > div > button" ).click();
         return extractBalance( valueOf( text ) );
     }
-//    public String getBalanceFinish() {
-//        return balanceFinish;
-//    }
 
     private int extractBalance(String text) {
         val start = text.indexOf(balanceStart);
