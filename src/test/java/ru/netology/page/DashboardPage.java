0f277a2651eb0fb7534ec117ea28bbb15f497e08
AlikAskat <a-r-b@bk.ru>
2020-08-18 14:01:33 +0500

    Update DashboardPage.java

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.SelenideElement;
 import lombok.val;
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.impl.Html.text;
@@ -28,6 +29,10 @@ public class DashboardPage {
         this.id = id;
         $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']").click();
         $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']").click();
+        $("[class='money-input__currency']");
+        $("[placeholder='0000 0000 0000 0000']");
+        $("[value='**** **** ****']");
+        $( byText( "Пополнить" ) ).click();
         return extractBalance( valueOf( text ) );
     }
 
