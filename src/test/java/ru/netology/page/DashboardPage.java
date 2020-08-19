dec708812f184102b47d1d8f48f0165216512ae4
AlikAskat <a-r-b@bk.ru>
2020-08-19 23:25:17 +0500

    Update DashboardPage.java

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -1,5 +1,6 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import lombok.val;
@@ -23,17 +24,17 @@ public class DashboardPage {
 
     public int getCardBalance(String id) {
         val text = cards.find(text(id)).getText();
-//        val string = balanceStart.equals("[label='Сумма']");
         $( byText( "Пополнить" ) ).click();
-        $("#root > div > form > fieldset > div:nth-child(1) > div > span > span > span.input__box > input").setValue( "100" );
-        $("#root > div > form > fieldset > div:nth-child(2) > span > span > span.input__box > input").setValue("'5559 0000 0000 0002");
+        $("[maxlength='14']").setValue( "100" );
+        $("[maxlength='19']").setValue("'5559 0000 0000 0002");
+        $("[class='App_appContainer__3jRx1']").waitUntil( Condition.visible, 15000);
 //        ("[data-test-id='amount']");
 //        ( "[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']" );
 //        $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']").click();
 //        $("[class='money-input__currency']");
 //        $("[placeholder='0000 0000 0000 0000']");
 //        $("[value='**** **** ****']");
-        $( byText( "Пополнить" ) ).click();
+        $( "[data-test-id='action-transfer']").click();
         return extractBalance(text);
     }
 
