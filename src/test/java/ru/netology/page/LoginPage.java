b58509f3c269623a0d445cc645519c3f10aa0923
AlikAskat <a-r-b@bk.ru>
2020-08-19 14:50:35 +0500

    Update LoginPage.java

--- src/test/java/ru/netology/page/LoginPage.java
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -2,12 +2,11 @@ package ru.netology.page;
 
 import ru.netology.data.DataHelper;
 
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
     public VerificationPage validLogin(DataHelper.AuthInfo info) {
-        $( byText( "Логин" ) ).setValue( info.getLogin() );
+        $( "[class='input__inner'] input" ).setValue( info.getLogin() );
         $( "[data-test-id='password'] input" ).setValue( info.getPassword() );
         $( "[data-test-id='action-login']" ).click();
         return new VerificationPage();
